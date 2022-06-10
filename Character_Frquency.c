7d564db77b92071d3cd5b1c7ef243f93e529cf88
Yokesh30042002 <107024513+Yokesh30042002@users.noreply.github.com>
2022-06-10 11:45:39 +0530

    Update Character_Frquency.c

--- Character_Frquency.c
+++ Character_Frquency.c
@@ -9,17 +9,36 @@
      
 */
 
-#include<stdio.h>
+#include <stdio.h>
+#define NUM(a) ((a>=48)&&(a<=57))
+
 int main()
 {
-//char a[100] = "a1b2c3";
-char a[100] = "d2f4c6";
-for(int i=0;a[i];i=i+2)
-{
-   for(int j=0;j<a[i+1]-'0';j++)
-   {
-       printf("%c",a[i]);
-   }
+    char s[50] = "a150b60c352";
+    char cp;
+    int num=0;
+    for(int i=0;s[i];i++)
+    {
+        if(!NUM(s[i]))
+        {
+            cp = s[i];
+            continue;
+        }
+        else
+        {
+            do
+            {
+                int temp = s[i] - '0';
+                num = (num*10)+temp;
+                i=i+1;
+            }while(NUM(s[i]));
+            i = i-1;
+                for(int j=0;j<num;j++)
+        printf("%c",cp);
+        }
+        num=0;
+    }
+
+    return 0;
 }
-return 0;
-}
+
