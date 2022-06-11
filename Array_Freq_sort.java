b59990d04f18a158e3d962f870b529f4b83a3891
Yokesh30042002 <107024513+Yokesh30042002@users.noreply.github.com>
2022-06-11 18:05:47 +0530

    Update Array_Freq_sort.java

--- Array_Freq_sort.java
+++ Array_Freq_sort.java
@@ -18,7 +18,9 @@ public class Main
     public static void main(String[] args)
     {
         //int a[] = {2,2,2,3,3,4,4,1,1,1,1};
-        int a[] = {1,2,5,2,5,6,7,8,5};
+        //int a[] = {9,9,9,9,1,2,5,2,5,6,7,8,5};
+        //int a[] = { 1, 2, 3, 34, 34, 2, 1, 34, 1, 1, 1 };
+        int a[] = { 2, 2, 2, 2, 5, 5, 5, 5, 5, 5, 67, 67, 67, 67, 67};
         HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
         for(int i=0;i<a.length;i++)
         {
@@ -27,14 +29,29 @@ public class Main
                 map.put(a[i],freq(a,a[i]));
             }
         }
+        int n=map.size();int j=0;
+        int[][] k = new int[2][n];
         for(Map.Entry<Integer,Integer> e:map.entrySet())
         {
-            for(int i=0;i<e.getValue();i++)
+            k[0][j] = e.getKey();
+            k[1][j++]=e.getValue();
+        }
+        frq_sort(k,n);
+        print_arr(k,n);
+        }
+    public static void frq_sort(int[][] k,int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+        for(int s=0;s<n;s++)
+        {
+            if(k[1][i]>k[1][s])
             {
-                System.out.print(e.getKey()+" ");
+                int temp1 = k[1][i];k[1][i] = k[1][s];k[1][s] = temp1;
+                int temp2 = k[0][i];k[0][i] = k[0][s];k[0][s] =temp2;
             }
         }
-        
+        }
     }
     public static int freq(int[] a,int key)
     {
@@ -47,5 +64,11 @@ public class Main
             }
         }
         return count;
+    }
+    public static void print_arr(int[][] k,int n)
+    {
+        for(int s=0;s<n;s++)
+        for(int j=0;j<k[1][s];j++)
+        System.out.print(k[0][s]+" ");
     }
 }
